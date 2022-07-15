e548f65bf492e9ea2b0c084358d054ea8b01a435
JenifferBarrios <jbarrios03@hotmail.com>
2022-07-15 11:51:49 -0500

    capture el valor de un input en js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+var boton_validar = document.getElementById("ev") ;
+console.log(boton_validar);
+
+
+
+
+function mostrarValor () {
+    var input_value = document.getElementById("nt").value;
+    console.log(input_value);
+  }
+  
+boton_validar.addEventListener("click",mostrarValor)
+
+
+
