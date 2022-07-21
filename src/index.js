9811ee620330e8ee331b738339db59f18aa4514c
JenifferBarrios <jbarrios03@hotmail.com>
2022-07-20 20:26:30 -0500

    primeros estilos en css

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import validator from './validator.js';
 console.log(validator);
 
 
-var boton_validar = document.getElementById("ev") ;
-console.log(boton_validar);
-
-
-
-
-function mostrarValor () {
-    var input_value = document.getElementById("nt").value;
-    console.log(input_value);
-  }
-  
+var boton_validar = document.getElementById("botonvalidar") ;
 boton_validar.addEventListener("click",mostrarValor)
 
-
-
+function mostrarValor () {
+    var input_value = document.getElementById("numerotarjeta").value;
+    
+    let ar= Array.from((input_value));
+    console.log (ar); 
+    let arrayNumero = []
+    for(let i=0; i<ar.length; i++ ){
+          const elemento = parseInt(ar[i])
+          arrayNumero.push(elemento)
+    }
+    console.log(arrayNumero)
+    
+}
