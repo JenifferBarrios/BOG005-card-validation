894fb85208d920875806e6175cd01de6c976acda
JenifferBarrios <jbarrios03@hotmail.com>
2022-07-22 14:06:03 -0500

    doble y suma de los mayores a diez

--- src/index.js
+++ src/index.js
@@ -5,17 +5,37 @@ console.log(validator);
 
 var boton_validar = document.getElementById("botonvalidar") ;
 boton_validar.addEventListener("click",mostrarValor)
-
+let ar=[];
 function mostrarValor () {
+    
     var input_value = document.getElementById("numerotarjeta").value;
     
-    let ar= Array.from((input_value));
+    ar= Array.from((input_value));
     console.log (ar); 
     let arrayNumero = []
+    /*
     for(let i=0; i<ar.length; i++ ){
           const elemento = parseInt(ar[i])
           arrayNumero.push(elemento)
     }
-    console.log(arrayNumero)
-    
+    */
+  let reversadelarreglo =ar.reverse();
+console.log(ar);
+let resultado=0;
+for (let i = 0; i < reversadelarreglo.length; i += 2) {
+var stringconvertidoanumero = parseInt(reversadelarreglo[i]);
+  var dobledelnumero= stringconvertidoanumero*2;
+if (dobledelnumero>=10) {
+let arraynumerodoble=String(dobledelnumero).split("")
+let sumanumerodoble = parseInt(arraynumerodoble[0]) + parseInt(arraynumerodoble[1])
+console.log(sumanumerodoble);
 }
+}
+console.log(resultado);
+
+
+return ar
+}
+
+
+
