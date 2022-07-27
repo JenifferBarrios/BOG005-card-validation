1f9907640a1c6a232ecb810a4d8d86dbd38a15e1
JenifferBarrios <jbarrios03@hotmail.com>
2022-07-27 12:50:58 -0500

    aqui cree una muktiplicacion *2 y saque los mayores que 10

--- src/index.js
+++ src/index.js
@@ -4,38 +4,47 @@ console.log(validator);
 
 
 var boton_validar = document.getElementById("botonvalidar") ;
-boton_validar.addEventListener("click",mostrarValor)
+boton_validar.addEventListener("click",mostrarValor) ;
 let ar=[];
-function mostrarValor () {
+function mostrarValor () { 
     
-    var input_value = document.getElementById("numerotarjeta").value;
-    
-    ar= Array.from((input_value));
-    console.log (ar); 
-    let arrayNumero = []
-    /*
-    for(let i=0; i<ar.length; i++ ){
-          const elemento = parseInt(ar[i])
-          arrayNumero.push(elemento)
-    }
-    */
+  var input_value = document.getElementById("numerotarjeta").value;
+  
+  ar= Array.from((input_value));
+  console.log (ar); 
+  let arrayNumero = []
+  /*
+  for(let i=0; i<ar.length; i++ ){
+        const elemento = parseInt(ar[i])
+        arrayNumero.push(elemento)
+  }
+  */
   let reversadelarreglo =ar.reverse();
-console.log(ar);
-let resultado=0;
-for (let i = 0; i < reversadelarreglo.length; i += 2) {
-var stringconvertidoanumero = parseInt(reversadelarreglo[i]);
-  var dobledelnumero= stringconvertidoanumero*2;
-if (dobledelnumero>=10) {
-let arraynumerodoble=String(dobledelnumero).split("")
-let sumanumerodoble = parseInt(arraynumerodoble[0]) + parseInt(arraynumerodoble[1])
-console.log(sumanumerodoble);
-}
-}
-console.log(resultado);
+  console.log(ar);
 
-
-return ar
-}
-
-
-
+  const resultadoArray=[];
+  const resultadoSuma = 0;
+  for (let i = 0; i < reversadelarreglo.length; i += 1) {
+    const stringconvertidoanumero = parseInt(reversadelarreglo[i]);
+    if (i % 2 === 0)  {
+      const dobledelnumero = stringconvertidoanumero * 2;
+      if (dobledelnumero >= 10) {
+        const arraynumerodoble = String(dobledelnumero).split("")
+        const sumanumerodoble = parseInt(arraynumerodoble[0]) + parseInt(arraynumerodoble[1])
+        resultadoArray.push(sumanumerodoble)
+      } else {
+        resultadoArray.push(stringconvertidoanumero)
+      }
+    } else {
+      resultadoArray.push(stringconvertidoanumero);
+    }
+     
+    };
+    console.log(resultadoArray);
+    //en resultadoarray estan todos lo numeros 
+  }
+  
+    // recorrer resultadoArray e ir sumando todos los elementos a resultadoSuma
+    resultadoSuma = resultadoArray.reduce()
+  // verificar si el resultadoSuma es multiplo de 10
+  // retornar verdadero o falso segun la verificacion previa
